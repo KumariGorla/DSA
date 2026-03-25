facad618bd4a2422ab4b1373ece7151146efa08c
kumarigorla <kumarigorla09@gmail.com>
2026-03-25 11:48:51 +0530

    Updated last two lines in Towers of Hanoi

--- varslidwind2.java
+++ varslidwind2.java
@@ -1,7 +1,11 @@
 public class varslidwind2 {
     public static void main(String[] args) {
         int arr[] = {1,0,0,0,1};
-        
+        System.out.println("Original array");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
     }
     
 }
